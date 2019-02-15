a50ea8a6a1a72aa17132686bb38070a5a3d31be9
kimseulbi <ksb5166@gmail.com>
2019-02-15 13:28:57 +0900

    웹반응형 적용 및 서브 페이지 추가

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import "@babel/polyfill";
+import { async } from "q";
 
 const templates = {
-  main: document.querySelector("#main").content
+  main: document.querySelector("#main").content,
+  baeminchan: document.querySelector("#baeminchan").content
 };
 const rootEl = document.querySelector(".container");
 const logoEl = document.querySelector(".logo");
@@ -16,7 +18,35 @@ logoEl.addEventListener("click", e => {
 async function drawMainPage() {
   // 1. 템플릿 복사
   const frag = document.importNode(templates.main, true);
+  // 2. 요소 선택
+  const baeminchanImgEl = frag.querySelector(".protfo__img--baeminchan");
+  // 5. 이벤트 리스너 등록하기
+  baeminchanImgEl.addEventListener("click", e => {
+    drawBaeminchanPage();
+  });
+
+  // const imgCoverEl = protfoImgEl.querySelector(".img-cover");
+
+  // 프로젝트 이미지 호버시 버튼 출력
+  // protfoImgEl.addEventListener("mouseover", function() {
+  //   imgCoverEl.style.opacity = "1";
+  // });
+  // protfoImgEl.addEventListener("mouseleave", function() {
+  //   imgCoverEl.style.opacity = "0";
+  // });
+
+  // 6. 템플릿을 문서에 삽입
   rootEl.textContent = "";
   rootEl.appendChild(frag);
 }
+
+// 배민찬 서브페이지
+async function drawBaeminchanPage() {
+  // 1. 템플릿 복사
+  const frag = document.importNode(templates.baeminchan, true);
+  // 6. 템플릿을 문서에 삽입
+  rootEl.textContent = "";
+  rootEl.appendChild(frag);
+}
+
 drawMainPage();
