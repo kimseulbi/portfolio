a4c66fc9eccd634b42a331ee2cb69ceb75469de1
kimseulbi <ksb5166@gmail.com>
2019-02-16 03:06:07 +0900

    배민찬 서브 페이지 작업 완료

--- dist/style.a6dae8f7.js
+++ dist/style.a6dae8f7.js
@@ -176,7 +176,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./images/logo.svg":[["logo.7a7149fb.svg","scss/images/logo.svg"],"scss/images/logo.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./images/logo.svg":[["logo.7a7149fb.svg","scss/images/logo.svg"],"scss/images/logo.svg"],"./images/chan_main.jpg":[["chan_main.c19e532d.jpg","scss/images/chan_main.jpg"],"scss/images/chan_main.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -203,7 +203,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61814" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49408" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import "@babel/polyfill";
-import { async } from "q";
 
 const templates = {
   main: document.querySelector("#main").content,
-  baeminchan: document.querySelector("#baeminchan").content
+  baeminchan: document.querySelector("#baeminchan").content,
+  nav: document.querySelector("#navigation").content
 };
+
 const rootEl = document.querySelector(".container");
 const logoEl = document.querySelector(".logo");
+const navEl = document.querySelector(".navigation");
 
 // 로고를 누르면 첫페이지로 이동
 logoEl.addEventListener("click", e => {
@@ -17,9 +19,10 @@ logoEl.addEventListener("click", e => {
 // 메인페이지
 async function drawMainPage() {
   // 1. 템플릿 복사
-  const frag = document.importNode(templates.main, true);
+  const frag1 = document.importNode(templates.nav, true);
+  const frag2 = document.importNode(templates.main, true);
   // 2. 요소 선택
-  const baeminchanImgEl = frag.querySelector(".protfo__img--baeminchan");
+  const baeminchanImgEl = frag2.querySelector(".protfo__img--baeminchan");
   // 5. 이벤트 리스너 등록하기
   baeminchanImgEl.addEventListener("click", e => {
     drawBaeminchanPage();
@@ -36,17 +39,22 @@ async function drawMainPage() {
   // });
 
   // 6. 템플릿을 문서에 삽입
+  navEl.textContent = "";
   rootEl.textContent = "";
-  rootEl.appendChild(frag);
+  navEl.appendChild(frag1);
+  rootEl.appendChild(frag2);
 }
 
 // 배민찬 서브페이지
 async function drawBaeminchanPage() {
   // 1. 템플릿 복사
-  const frag = document.importNode(templates.baeminchan, true);
+  const frag1 = document.importNode(templates.nav, false);
+  const frag2 = document.importNode(templates.baeminchan, true);
   // 6. 템플릿을 문서에 삽입
+  navEl.textContent = "";
   rootEl.textContent = "";
-  rootEl.appendChild(frag);
+  navEl.appendChild(frag1);
+  rootEl.appendChild(frag2);
 }
 
 drawMainPage();
