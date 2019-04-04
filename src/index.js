3c023958e2c4c78e49d1523a0b89c112907288bb
kimseulbi <ksb5166@gmail.com>
2019-04-05 07:06:44 +0900

    포폴 추가및 이미지 최적화

--- src/index.js
+++ src/index.js
@@ -7,19 +7,22 @@ const templates = {
   baseball: document.querySelector("#baseball").content,
   colorChallenge: document.querySelector("#colorChallenge").content,
   focs: document.querySelector("#focs").content,
+  fx: document.querySelector("#fx").content,
+  wx: document.querySelector("#wx").content,
+  smart: document.querySelector("#smart").content,
   nav: document.querySelector("#navigation").content
 };
 
 const rootEl = document.querySelector(".container");
-const logoEl = document.querySelector(".logo");
+// const logoEl = document.querySelector(".logo");
 const navEl = document.querySelector(".navigation");
 let counter = 0;
 
 // 로고를 누르면 첫페이지로 이동
-logoEl.addEventListener("popstate", e => {
-  window.history.pushState(counter, "", "");
-  drawMainPage();
-});
+// logoEl.addEventListener("popstate", e => {
+//   window.history.pushState(counter, "", "");
+//   drawMainPage();
+// });
 
 // 메인페이지
 async function drawMainPage() {
@@ -31,6 +34,10 @@ async function drawMainPage() {
   const baeminImgEl = frag2.querySelector(".protfo__img--baemin");
   const baseballImgEl = frag2.querySelector(".protfo__img--baseball");
   const colorChallengeImgEl = frag2.querySelector(".protfo__img--rgb");
+  const focsImgEl = frag2.querySelector(".protfo__img--focs");
+  const fxImgEl = frag2.querySelector(".protfo__img--fx");
+  const wxImgEl = frag2.querySelector(".protfo__img--wx");
+  const smartImgEl = frag2.querySelector(".protfo__img--smart");
   window.scroll({
     top: 0,
     left: 0,
@@ -52,6 +59,22 @@ async function drawMainPage() {
   colorChallengeImgEl.addEventListener("click", e => {
     history.pushState(counter++, "", "colorChallenge");
     drawColorChallengePage();
+  });
+  focsImgEl.addEventListener("click", e => {
+    history.pushState(counter++, "", "focs");
+    drawFocsPage();
+  });
+  fxImgEl.addEventListener("click", e => {
+    history.pushState(counter++, "", "firesightFX");
+    drawFxPage();
+  });
+  wxImgEl.addEventListener("click", e => {
+    history.pushState(counter++, "", "firesightWX");
+    drawWxPage();
+  });
+  smartImgEl.addEventListener("click", e => {
+    history.pushState(counter++, "", "AirSCANsmart");
+    drawSmartPage();
   });
 
   // 6. 템플릿을 문서에 삽입
@@ -117,6 +140,74 @@ async function drawColorChallengePage() {
   // 1. 템플릿 복사
   const frag1 = document.importNode(templates.nav, false);
   const frag2 = document.importNode(templates.colorChallenge, true);
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+  // 6. 템플릿을 문서에 삽입
+  navEl.textContent = "";
+  rootEl.textContent = "";
+  navEl.appendChild(frag1);
+  rootEl.appendChild(frag2);
+}
+
+// focs 서브페이지
+async function drawFocsPage() {
+  // 1. 템플릿 복사
+  const frag1 = document.importNode(templates.nav, false);
+  const frag2 = document.importNode(templates.focs, true);
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+  // 6. 템플릿을 문서에 삽입
+  navEl.textContent = "";
+  rootEl.textContent = "";
+  navEl.appendChild(frag1);
+  rootEl.appendChild(frag2);
+}
+
+// fx 서브페이지
+async function drawFxPage() {
+  // 1. 템플릿 복사
+  const frag1 = document.importNode(templates.nav, false);
+  const frag2 = document.importNode(templates.fx, true);
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+  // 6. 템플릿을 문서에 삽입
+  navEl.textContent = "";
+  rootEl.textContent = "";
+  navEl.appendChild(frag1);
+  rootEl.appendChild(frag2);
+}
+
+// wx 서브페이지
+async function drawWxPage() {
+  // 1. 템플릿 복사
+  const frag1 = document.importNode(templates.nav, false);
+  const frag2 = document.importNode(templates.wx, true);
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+  // 6. 템플릿을 문서에 삽입
+  navEl.textContent = "";
+  rootEl.textContent = "";
+  navEl.appendChild(frag1);
+  rootEl.appendChild(frag2);
+}
+
+// smart 서브페이지
+async function drawSmartPage() {
+  // 1. 템플릿 복사
+  const frag1 = document.importNode(templates.nav, false);
+  const frag2 = document.importNode(templates.smart, true);
   window.scroll({
     top: 0,
     left: 0,
