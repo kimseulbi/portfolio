631c3b2f31e24d31b2bc7bed0d4d0ec30a0650f7
kimseulbi <ksb5166@gmail.com>
2021-02-21 01:51:27 +0900

    webpack 적용 제거

--- src/postcss.config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-  parser: "postcss-scss",
-  plugins: [
-    require("postcss-flexbugs-fixes"),
-    require('autoprefixer')({ browsers: 'last 2 versions, not ie <= 11, not ie_mob <= 11', grid : false }),
-    require("postcss-fail-on-warn"),
-  ],
-};
